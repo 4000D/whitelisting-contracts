6ff2ead21178851168d303798184e9c6ec995289
4000D <dkdkajej@gmail.com>
2021-04-28 21:12:56 +0900

    update server

--- server/controller.js
+++ server/controller.js
@@ -4,8 +4,10 @@ const express = require("express");
 const { toBN } = require("web3-utils");
 const orderBy = require("lodash/orderBy");
 const zipObject = require("lodash/zipObject");
+const { isValidAddress } = require("ethereumjs-util");
 
 const { Leaf, Tree } = require("../lib");
+const { appendHexPrefix, DEFAULT_AMOUNT } = require("../lib");
 
 const router = express.Router();
 
@@ -22,8 +24,8 @@ router.use(
 
     const n = await client.db().collection("merkleTree").countDocuments({});
 
-    if (res.locals.trees && res.locals.trees.length === n) {
-      return next();
+    if (req.app.locals.trees && req.app.locals.trees.length === n) {
+      return;
     }
 
     /** @type {Tree[]} */
@@ -48,14 +50,40 @@ router.use(
       })
       .filter((v) => v);
 
-    res.locals.trees = trees;
+    req.app.locals.trees = trees;
+  })
+);
+
+router.get("/roots", (req, res) => {
+  const { trees } = req.app.locals;
+  res.json({
+    success: true,
+    roots: trees.map((tree) => tree.getRootHex()),
+  });
+});
+
+router.get(
+  "/roots/:root",
+  catchAsync(async (req, res) => {
+    const root = appendHexPrefix(req.params.root);
+    const { trees } = req.app.locals;
+    const tree = trees.find((tree) => tree.getRootHex() === root);
+
+    if (!tree) {
+      return res.json({ success: false, message: `Unknown root: ${root}` });
+    }
+
+    return res.json({
+      success: true,
+      accounts: tree.leaves.map((leaf) => leaf.getAddressHex()),
+    });
   })
 );
 
 // router.get(
 //   "/trees",
 //   catchAsync(async (req, res) => {
-//     const { trees } = res.locals;
+//     const { trees } = req.app.locals;
 
 //     const trees = trees.map((treeInfo) => {
 //       const leafInfos = treeInfo.leafInfos;
@@ -100,7 +128,7 @@ router.get(
     const indexMap = zipObject(targetRoots, rootIndex);
 
     /** @type {Tree[]} */
-    const { trees } = res.locals;
+    const { trees } = req.app.locals;
 
     const data = trees
       .filter((tree) => targetRootsSet.has(tree.root.toString("hex")))
@@ -129,6 +157,82 @@ router.get(
       root: "0x" + tree.root.toString("hex"),
       amount: leaf.amount.toString(),
       proof: tree.getProof(leaf),
+    });
+  })
+);
+
+router.post(
+  "/accounts",
+  catchAsync(async (req, res) => {
+    const { accounts = [] } = req.body;
+
+    /** @type {MongoClient} */
+    const client = req.app.locals.client;
+
+    if (accounts.length === 0) {
+      return res.json({
+        success: false,
+        message: "body.leaves must be non-empty array",
+      });
+    }
+
+    for (const account of accounts) {
+      if (!isValidAddress(account)) {
+        return res.json({
+          success: false,
+          message: `account ${account} is not valid address`,
+        });
+      }
+    }
+
+    /** @type {Leaf[]} */
+    const leaves = accounts.map(
+      (account) => new Leaf(account, toBN(DEFAULT_AMOUNT))
+    );
+
+    const tree = new Tree(leaves);
+
+    const root = tree.root.toString("hex");
+
+    const n = await client
+      .db()
+      .collection("merkleTree")
+      .countDocuments({ root });
+
+    if (n > 0) {
+      return res.json({
+        success: false,
+        message: `tree#${root} already exists`,
+      });
+    }
+
+    const leafInfos = leaves.map((leaf) => ({
+      address: leaf.getAddressHex().toLowerCase(),
+      amount: leaf.getAmountBN().toString(10),
+    }));
+
+    const data = { root, leafInfos };
+    await client.db().collection("merkleTree").insertOne(data);
+
+    await Promise.all(
+      leaves.map((leaf, index) => {
+        const address = leaf.getAddressHex().toLowerCase();
+        return client.db().collection("address").findOneAndUpdate(
+          { address },
+          {
+            $push: {
+              root,
+              index,
+            },
+          },
+          { upsert: true }
+        );
+      })
+    );
+
+    return res.json({
+      success: true,
+      root: appendHexPrefix(root),
     });
   })
 );
--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || "17172";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.use("/", require("./controller"));
 
